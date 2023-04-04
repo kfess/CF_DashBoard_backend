65ccc6fce96ebcb5b7ae43f1eeb945f22992426d
kita456 <g140702@gmail.com>
2023-04-04 21:59:07 +0900

    add directories

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import { ApolloServer } from "apollo-server";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
