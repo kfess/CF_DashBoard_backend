a8f8d5f0ae68aaf86330abb6c6fd6532ab3b7283
kita456 <g140702@gmail.com>
2023-04-08 23:55:22 +0900

    add merge contest and problems

--- src/usecases/FetchContestsInteractor.ts
+++ src/usecases/FetchContestsInteractor.ts
@@ -22,7 +22,16 @@ export class FetchContestsInteractor implements FetchContestUsecase {
     this.contestRepository = contestRepository;
   }
 
-  async run(): Promise<Contest[]> {}
+  async run(): Promise<Contest[]> {
+    const officialContests = await this.fetchContestsFromCodeforcesAPI();
+    const [officialProblems, officialStatisticsProblems] =
+      await this.fetchProblemsFromCodeforcesAPI();
+    const mergedContests = this.mergeContestsWithProblems(
+      officialContests,
+      officialProblems,
+      officialStatisticsProblems
+    );
+  }
 
   private async fetchContestsFromCodeforcesAPI(): Promise<OfficialContest[]> {
     try {
@@ -105,95 +114,21 @@ export class FetchContestsInteractor implements FetchContestUsecase {
       });
     });
 
+    // Group problems by contestId
+    const problemMap: Map<number, Problem[]> = new Map();
+    problems.forEach((problem) => {
+      const contestId = problem.contestId;
+      const problems = problemMap.get(contestId) || [];
+      problems.push(problem);
+      problemMap.set(contestId, problems);
+    });
+
     return officialContests.map((contest) => {
       return new Contest({
         ...contest,
-        problems,
+        problems: problemMap.get(contest.id) || [],
         classification: getClassification(contest.name),
       });
     });
   }
-
-  //   private async fetchContestsFromCodeforcesAPI(): Promise<Contest[]> {
-  //     try {
-  //       const response = await axios.get(CF_CONTESTS_URL);
-  //       const validationResult = ContestApiResponseSchema.safeParse(
-  //         response.data
-  //       );
-
-  //       if (!validationResult.success) {
-  //         throw new Error(
-  //           `Failed to validate contest data from Codeforces API. Error: ${validationResult.error}`
-  //         );
-  //       }
-
-  //       const contestsData = validationResult.data.result;
-  //       return contestsData.map((contestData) => {
-  //         return new Contest({
-  //           ...contestData,
-  //           contestId: contestData.id,
-  //           contestName: contestData.name,
-  //         });
-  //       });
-  //     } catch (error) {
-  //       console.error(`Failed to fetch contests from Codeforces API: ${error}`);
-  //       return [];
-  //     }
-  //   }
-
-  //   private async fetchProblemsFromCodeforcesAPI(
-  //     contests: OfficialContest[]
-  //   ): Promise<Problem[]> {
-  //     try {
-  //       const response = await axios.get(CF_PROBLEMS_URL);
-  //       if (response.status !== 200) {
-  //         throw new Error(
-  //           `Failed to fetch problems from Codeforces API. Status code: ${response.status}`
-  //         );
-  //       }
-
-  //       const validatedResponse = ProblemApiResponseSchema.safeParse(
-  //         response.data
-  //       );
-  //       if (!validatedResponse.success) {
-  //         throw new Error(
-  //           `Failed to validate problem data from Codeforces API. Error: ${validatedResponse.error}`
-  //         );
-  //       }
-
-  //       const problemsData = validatedResponse.data.result.problems;
-  //       const problemsStatisticsData =
-  //         validatedResponse.data.result.problemStatistics;
-
-  //       const contestMap = new Map(
-  //         contests.map((contest) => [contest.id, contest.name])
-  //       );
-
-  //       const statisticsMap = new Map(
-  //         problemsStatisticsData.map((stat) => [
-  //           `${stat.contestId}-${stat.index}`,
-  //           stat.solvedCount,
-  //         ])
-  //       );
-
-  //       return problemsData.map((problemData) => {
-  //         const solvedCount = statisticsMap.get(
-  //           `${problemData.contestId}-${problemData.index}`
-  //         );
-  //         const contestName = contestMap.get(problemData.contestId) ?? "";
-  //         const classification = getClassification(contestName ?? "");
-
-  //         return new Problem({
-  //           ...problemData,
-  //           solvedCount: solvedCount,
-  //           contestName: contestName,
-  //           classification: classification,
-  //           tags: [...problemData.tags],
-  //         });
-  //       });
-  //     } catch (error) {
-  //       console.error(`Failed to fetch problems from Codeforces API: ${error}`);
-  //       return [];
-  //     }
-  //   }
 }
