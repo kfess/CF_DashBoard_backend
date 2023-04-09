bc21aee71f532225eea914e574fac78fb603bb2c
kita456 <g140702@gmail.com>
2023-04-09 19:05:05 +0900

    add jsdoc

--- src/usecases/GetContestsUsecase.ts
+++ src/usecases/GetContestsUsecase.ts
@@ -1,6 +1,16 @@
 import { Contest } from "@/entities/Contest";
 
 export interface GetContestsUsecase {
+  /**
+   * Gets a contest object from the backend database
+   * @param {number} id - The ID of the contest to find.
+   * @returns {Promise<Contest | null>} A promise that resolves to a contest object or null
+   */
   getContest(id: number): Promise<Contest | null>;
+
+  /**
+   * Gets a list of contests from the backend database
+   * @returns {Promise<Contest[]>} A promise that resolves to an array of contest objects
+   */
   getAllContests(): Promise<Contest[]>;
 }
