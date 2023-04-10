848ffd929329985001ec07349c1c3f7e2a0c145b
kita456 <g140702@gmail.com>
2023-04-10 23:43:52 +0900

    worked!

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import "module-alias/register";
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./graphql/schema/index";
 import { resolvers } from "./graphql/resolvers";
+import { PrismaContestRepository } from "./repositories/prisma/PrismaContestRepository";
+import { GetContestsInteractor } from "./usecases/GetContestsInteractor";
+
+const prisma = new PrismaClient();
+const contestRepository = new PrismaContestRepository(prisma);
+const getContestsUsecase = new GetContestsInteractor(contestRepository);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {},
+  context: {
+    getContestsUsecase,
+  },
 });
 
 server.listen().then(({ url }) => {
