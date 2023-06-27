5eb9d4ac4a3c5eaea9283ccf7cdb037e46f8a48f
kita456 <g140702@gmail.com>
2023-06-28 00:18:23 +0900

    update daily_update.ts

--- src/daily_update.ts
+++ src/daily_update.ts
@@ -1,19 +1,32 @@
+// Purpose: Lambda function to update the database with the latest contests from Codeforces
+// This function is supposed to be run on Lambda every a few hours
+
 import { PrismaClient } from "@prisma/client";
 import { PrismaContestRepository } from "./repositories/prisma/PrismaContestRepository";
 import { FetchContestsInteractor } from "./usecases/FetchContestsInteractor";
 
-async function main() {
-  const prisma = new PrismaClient();
-  const contestRepository = new PrismaContestRepository(prisma);
-  const fetchContestsInteractor = new FetchContestsInteractor(
-    contestRepository
-  );
-  await fetchContestsInteractor.fetchAllAndUpdate();
+// These lines are outside of the handler to take advantage of cached execution environments
+// https://docs.aws.amazon.com/lambda/latest/dg/running-lambda-code.html
+const prisma = new PrismaClient();
+const contestRepository = new PrismaContestRepository(prisma);
+const fetchContestsInteractor = new FetchContestsInteractor(contestRepository);
 
+export async function main() {
+  await fetchContestsInteractor.fetchAllAndUpdate();
   await prisma.$disconnect();
 }
 
-main().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
+// Triggered by Lambda function every a few hours
+exports.handler = async (event: any, context: any) => {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+// Execute the main function if this file is run directly
+if (require.main === module) {
+  main().catch(console.error);
+}
