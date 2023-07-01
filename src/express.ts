a76bf7be82f533b5c630360fdde9fb52d042397d
kita456 <g140702@gmail.com>
2023-07-01 15:06:59 +0900

    rename entrypoint file

---
+++ src/express.ts
@@ -0,0 +1,35 @@
+import { checkEnvVars } from "./utils/checkEnvVars";
+import serverlessExpress from "@vendia/serverless-express";
+import compression from "compression";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import router from "./routes";
+
+checkEnvVars();
+
+const app = express();
+
+app.use(
+  compression({
+    level: 6,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(router);
+
+// Execute the main function if this file is run directly
+if (require.main === module) {
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+} else {
+  // Export the handler for Lambda function
+  const handler = serverlessExpress({ app });
+  exports.handler = async (event: any, contest: any) => {
+    return await handler(event, contest);
+  };
+}
