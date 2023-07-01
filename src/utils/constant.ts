8ee9e6b41e1d683280bd9c6d021593e432daa4cd
kita456 <g140702@gmail.com>
2023-07-01 13:01:18 +0900

    update cf contest url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,2 +1,7 @@
-export const CF_CONTESTS_URL = "https://codeforces.com/api/contest.list";
-export const CF_PROBLEMS_URL = "https://codeforces.com/api/problemset.problems";
+// if env variable is not set, use default value
+export const CF_CONTESTS_URL =
+  process.env.CF_CONTESTS_URL || "https://codeforces.com/api/contest.list";
+
+export const CF_PROBLEMS_URL =
+  process.env.CF_PROBLEMS_URL ||
+  "https://codeforces.com/api/problemset.problems";
