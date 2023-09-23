ce6c9389f5a270442b98a1f45cc1c82c00395917
kita456 <g140702@gmail.com>
2023-09-24 01:41:13 +0900

    add new files

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,29 @@
+## Overview
+
+In this section, please briefly explain the purpose and summary of this PR.
+
+## Changes
+
+In this section, please list the specific changes and modifications in bullet points.
+
+- Change 1
+- Change 2
+- Change 3
+
+## Impact
+
+In this section, please describe the scope of impact of this PR and any effects it might have on other functionalities.
+
+## Testing
+
+In this section, please outline the test cases and testing methods related to this PR.
+
+- Test Case 1
+- Test Case 2
+- Test Case 3
+
+## Related Issues
+
+In this section, please link any related Issues or tasks. Write it as follows:
+
+- Related Issue: #123
---
+++ jest.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageReporters: ["text", "lcov"],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx,js,jsx}",
+    "!src/**/index.{ts,js}",
+    "!src/**/*.(spec|test).{ts,tsx,js,jsx}",
+    "!src/**/*mock.{ts,tsx,js,jsx}",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+export default config;
