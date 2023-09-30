78677ed41df3c7141b4ef243e1e058d24bb07924
kita456 <g140702@gmail.com>
2023-09-30 17:14:09 +0900

    add disbale x-powered-by

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(compression({ level: 6 }));
 app.use(helmet());
+app.disable('x-powered-by');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
